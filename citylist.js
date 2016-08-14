1835074483c56a108f5542c91cc062bafac2f3e0
anu0012 <anurag30671371@gmail.com>
2016-08-14 17:09:08 +0530

    Final commit

---
+++ citylist.js
@@ -0,0 +1,253 @@
+var citymap = {
+        agra: {
+          center: {lat: 27.1767, lng: 78.0081},
+          death:856
+        },
+        
+       
+        ahmedabad: {
+          center: {lat: 23.0225, lng: 72.5714},
+          death: 1850
+        },
+        
+        
+        allahabad: {
+          center: {lat:25.4358 , lng:81.8463 },
+          death:933
+        },
+        
+        amritsar: {
+          center: {lat:31.6340 , lng: 74.8723},
+          death:152
+        },
+        
+       
+        asansol: {
+          center: {lat:23.67 , lng:86.95 },
+          death: 380
+        },
+        
+        aurangabad: {
+          center: {lat:24.04 , lng: 83.9},
+          death: 715
+        },
+        
+        bengaluru: {
+          center: {lat:12.97 , lng:77.56},
+          death:5508
+        },
+        
+        bhopal: {
+          center: {lat: 23.2599, lng: 77.4126},
+          death: 3623
+        },
+        
+        chandigarh: {
+          center: {lat:30.74 , lng:76.76},
+          death:419
+        },
+        
+        chennai: {
+          center: {lat: 13.07, lng: 80.24},
+          death:9663
+        },
+        
+        coimbatore: {
+          center: {lat: 11.00, lng: 76.96},
+          death:1128
+        },
+        
+        
+        delhi: {
+          center: {lat: 28.7041, lng: 77.1025},
+          death:6937
+        },
+        
+        dhanbad: {
+          center: {lat:23.7957 , lng: 86.4304},
+          death:144
+        },
+        
+        faridabad: {
+          center: {lat:28.395403 , lng: 77.315292},
+          death:711
+        },
+        
+        ghaziabad: {
+          center: {lat:28.667856 , lng: 77.449791},
+          death:850
+        },
+        
+        gwalior: {
+          center: {lat: 26.2183, lng: 78.1828},
+          death:1974
+        },
+        
+        hyderabad: {
+          center: {lat: 17.387140, lng: 78.491684},
+          death:2577
+        },
+        
+        indore: {
+          center: {lat:22.719568 , lng: 75.857727},
+          death: 5433
+        },
+
+        jabalpur: {
+          center: {lat: 23.168770, lng: 79.931847},
+          death:2919
+        },
+        
+        jaipur: {
+          center: {lat:26.92207 , lng: 75.778885},
+          death:1792
+        },
+        
+        jamshedpur: {
+          center: {lat:	22.805618 , lng: 86.203110},
+          death:424
+        },
+        
+        jodhpur: {
+          center: {lat:26.263863 , lng: 73.008957},
+          death:533
+        },
+        
+        kannur: {
+          center: {lat:11.874477 , lng: 75.370369},
+          death:1257
+        },
+        
+        khozikode: {
+          center: {lat: 11.25, lng: 75.77},
+          death:1270
+        },
+
+        kanpur: {
+          center: {lat:26.449923 , lng: 80.331871},
+          death:1257
+        },
+        
+        kochi: {
+          center: {lat: 9.939093, lng: 76.270523},
+          death:2280
+        },
+        
+        kolkata: {
+          center: {lat:22.572645 , lng: 88.363892},
+          death:3937
+        },
+        
+        kollam: {
+          center: {lat:8.88 , lng: 76.6},
+          death:1763
+        },
+        
+        kota: {
+          center: {lat:25.2138 , lng: 75.8648},
+          death:601
+        },
+        
+        lucknow: {
+          center: {lat:26.8467 , lng: 80.9462},
+          death:1202
+        },
+        
+        ludhiana: {
+          center: {lat:30.900965 , lng: 75.857277},
+          death:430
+        },
+        
+        madurai: {
+          center: {lat:	9.939093 , lng: 78.121719},
+          death:839
+        },
+        
+        mallapuram: {
+          center: {lat:11.04 , lng: 76.08},
+          death:2711
+        },
+        
+        meerut: {
+          center: {lat: 28.984644, lng: 77.705956},
+          death:964
+        },
+        
+        mumbai: {
+          center: {lat:19.0760 , lng: 72.8777},
+          death:24592
+        },
+        
+        nagpur: {
+          center: {lat:21.146633 , lng: 79.088860},
+          death:1083
+        },
+        
+        nashik: {
+          center: {lat:19.997454 , lng: 73.789803},
+          death:1023
+        },
+        
+        patna: {
+          center: {lat: 25.612677, lng: 85.158875},
+          death:1185
+        },
+        
+        pune: {
+          center: {lat: 18.516726, lng: 73.856255},
+          death:1624
+        },
+        
+        raipur: {
+          center: {lat:30.316700 , lng: 78.099998},
+          death:2222
+        },
+        
+        rajkot: {
+          center: {lat: 22.308155, lng: 70.800705},
+          death:981
+        },
+        
+        srinagar: {
+          center: {lat:30.221399 , lng: 78.780045},
+          death:511
+        },
+        
+        surat: {
+          center: {lat: 21.1702, lng: 72.8311},
+          death:1146
+        },
+        
+        thiruvanthapuram: {
+          center: {lat:8.5241 , lng: 76.9366},
+          death:1933
+        },
+        
+        thricchur: {
+          center: {lat:10.52 , lng:76.21},
+          death:1288
+        },
+        
+        thirichirapalli: {
+          center: {lat: 10.7905, lng: 78.7047},
+          death:778
+        },
+        
+        vadodra: {
+          center: {lat:22.3072 , lng:73.1812},
+          death:1196
+        },
+        
+        varanasi: {
+          center: {lat:25.321684, lng: 82.987289},
+          death:379
+        },
+        
+        vijaywada: {
+          center: {lat:16.5062 , lng: 80.6480},
+          death:1600
+        }
+        
+       
+        
+      };
